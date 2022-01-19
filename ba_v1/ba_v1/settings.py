f1576ebf7f5a7cbaca85423a80045931be1cfb83
Melissa Mejía Villamil <mejiavillamil232@gmail.com>
2022-01-18 20:31:07 -0500

    Eliminación temporal de la app de streaming para pruebas con robot 22/enero

--- ba_v1/ba_v1/settings.py
+++ ba_v1/ba_v1/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'chat',
     'login',
-    'streamapp',
+    # 'streamapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
