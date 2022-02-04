b71c854a3e31e79fbcfc7561014fefbad7aa5c22
Melissa Mejía Villamil <mejiavillamil232@gmail.com>
2022-02-03 20:10:04 -0500

    Se descomentan los valores relativos a la app streaming para pruebas a la conexión del Baxter

--- ba_v1/streamapp/views.py
+++ ba_v1/streamapp/views.py
@@ -1,29 +1,29 @@
-# from django.shortcuts import render
-# from django.http.response import StreamingHttpResponse
-# from streamapp.camera import VideoCamera
-# from streamapp.camera import IPWebCam
+from django.shortcuts import render
+from django.http.response import StreamingHttpResponse
+from streamapp.camera import VideoCamera
+from streamapp.camera import IPWebCam
 
-# # Create your views here.
+# Create your views here.
 
-# # Pintar el html donde saldrá el video
-
-
-# def index(request):
-#     return render(request, 'streamapp/home_video.html')
-
-# # Creamos un método para la cámara que se vaya a utilizar
+# Pintar el html donde saldrá el video
 
 
-# def gen(camera):
-#     while True:
-#         frame = camera.get_frame()
-#         yield (b'--frame\r\n'
-#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+def index(request):
+    return render(request, 'streamapp/home_video.html')
+
+# Creamos un método para la cámara que se vaya a utilizar
 
 
-# def video_feed(request):
-#     return StreamingHttpResponse(gen(VideoCamera()), content_type='multipart/x-mixed-replace; boundary= frame')
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-# def webcam_feed(request):
-#     return StreamingHttpResponse(gen(IPWebCam()), content_type='multipart/x-mixed-replace; boundary= frame')
+def video_feed(request):
+    return StreamingHttpResponse(gen(VideoCamera()), content_type='multipart/x-mixed-replace; boundary= frame')
+
+
+def webcam_feed(request):
+    return StreamingHttpResponse(gen(IPWebCam()), content_type='multipart/x-mixed-replace; boundary= frame')
