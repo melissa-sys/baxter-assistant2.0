07bfc0be5e64b623761f87e0105e2596b8b61935
Melissa Mejía Villamil <mejiavillamil232@gmail.com>
2022-03-29 12:29:04 -0500

    Actualización IP instancia EC2

--- ba_v1/ba_v1/settings.py
+++ ba_v1/ba_v1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ey3ttw!1(njuu3j@%$$x_4n6!=%0*8$nc7ci^#ni6z0_k=4*=q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '54.91.105.98']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '3.91.42.221']
 
 
 # Application definition
