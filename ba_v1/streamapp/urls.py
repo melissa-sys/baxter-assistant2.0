40e94a3dbcb1200f05664085e33a2380e14eb524
Melissa Mejía Villamil <mejiavillamil232@gmail.com>
2022-04-06 22:10:04 -0500

    Se añaden los archivos correspondientes de Kinesis con el fin de realizar pruebas con la plantilla cruda del WerbRTC

--- ba_v1/streamapp/urls.py
+++ ba_v1/streamapp/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from django.urls import include
 from streamapp import views
 
+from .views import KinesisView
+
 app_name = 'streaming'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('video_feed', views.video_feed, name='video_feed'),
+    path('video_feed', KinesisView.as_view(), name='video_feed'),
     # path('video_feed', views.video_feed, name='video_feed'),  # laptop
     # path('webcam_feed', views.webcam_feed, name='webcam_feed'),  # mobile
 
--- ba_v1/streamapp/views.py
+++ ba_v1/streamapp/views.py
@@ -2,6 +2,8 @@ from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http.response import StreamingHttpResponse
 
+from django.views.generic import TemplateView
+
 import cv2
 import socket
 import numpy as np
@@ -20,41 +22,43 @@ def index(request):
 
 # Creamos un método para la cámara que se vaya a utilizar
 
+class KinesisView(TemplateView):
+    template_name= 'streamapp/video.html'
 
-class Video(object):
-    def get_frame(self):
-        BUFF_SIZE = 65536
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        client_socket.setsockopt(
-            socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
-        host_name = socket.gethostname()
-        host_ip = '192.168.1.60'  # socket.gethostbyname(host_name)
-        print(host_ip)
-        port = 9999
-        message = b'Hello'
-        client_socket.sendto(message, (host_ip, port))
-        fps, st, frames_to_count, cnt = (0, 0, 20, 0)
+# class Video(object):
+#     def get_frame(self):
+#         BUFF_SIZE = 65536
+#         client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#         client_socket.setsockopt(
+#             socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
+#         host_name = socket.gethostname()
+#         host_ip = '192.168.1.60'  # socket.gethostbyname(host_name)
+#         print(host_ip)
+#         port = 9999
+#         message = b'Hello'
+#         client_socket.sendto(message, (host_ip, port))
+#         fps, st, frames_to_count, cnt = (0, 0, 20, 0)
 
-        while (True):
-            # print('melos')
-            packet, _ = client_socket.recvfrom(BUFF_SIZE)
-            data = base64.b64decode(packet, ' /')
-            npdata = np.fromstring(data, dtype=np.uint8)
-            frame = cv2.imdecode(npdata, 1)
-            cv2.imshow("RECEIVING VIDEO", frame)
-            ret, jpeg = cv2.imencode('.jpg', frame)
-            r = jpeg.tobytes()
+#         while (True):
+#             # print('melos')
+#             packet, _ = client_socket.recvfrom(BUFF_SIZE)
+#             data = base64.b64decode(packet, ' /')
+#             npdata = np.fromstring(data, dtype=np.uint8)
+#             frame = cv2.imdecode(npdata, 1)
+#             cv2.imshow("RECEIVING VIDEO", frame)
+#             ret, jpeg = cv2.imencode('.jpg', frame)
+#             r = jpeg.tobytes()
 
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord('q'):
-                client_socket.close()
-                break
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + r + b'\r\n\r\n')
+#             key = cv2.waitKey(1) & 0xFF
+#             if key == ord('q'):
+#                 client_socket.close()
+#                 break
+#             yield (b'--frame\r\n'
+#                    b'Content-Type: image/jpeg\r\n\r\n' + r + b'\r\n\r\n')
 
 
-def video_feed(request):
-    return StreamingHttpResponse(Video().get_frame(), content_type='multipart/x-mixed-replace; boundary= frame')
+# def video_feed(request):
+#     return StreamingHttpResponse(Video().get_frame(), content_type='multipart/x-mixed-replace; boundary= frame')
 
 
 # def webcam_feed(request):
