23aad6133bb37334547f0f955a8120d8818751a6
Melissa Mejía Villamil <mejiavillamil232@gmail.com>
2022-04-10 17:21:55 -0500

    Se corrigen variables del front que no serán ingresadas por el usuario pero que serán enviadas por medio del formulario en la vista que este tiene

--- ba_v1/streamapp/urls.py
+++ ba_v1/streamapp/urls.py
@@ -8,8 +8,6 @@ app_name = 'streaming'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('video_feed', KinesisView.as_view(), name='video_feed'),
-    # path('video_feed', views.video_feed, name='video_feed'),  # laptop
-    # path('webcam_feed', views.webcam_feed, name='webcam_feed'),  # mobile
+    path('video_feed', KinesisView.as_view(), name='video_feed')
 
 ]
